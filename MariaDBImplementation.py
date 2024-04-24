6cf668e7900e3a2f8a46aff527edd7f7a009ed09
Frantic9 <franticnine2015@gmail.com>
2024-04-24 18:45:28 -0500

    Added a check_user function for login

--- MariaDBImplementation.py
+++ MariaDBImplementation.py
@@ -7,21 +7,37 @@ class MariaDBImplementation:
         self.USER = 'admin'
         self.PASSWORD = 'SASMpass'
         self.DATABASE = 'SASM'
-        self.connection = pymysql.connect(host=self.HOST, user=self.USER, password=self.PASSWORD, db=self.DATABASE)
 
-    def _fetch_version():
-        with self.connection:
-            cur = self.connection.cursor()
+    def _fetch_version(self):
+        connection = pymysql.connect(host=self.HOST, user=self.USER, password=self.PASSWORD, db=self.DATABASE)
+
+        with connection:
+            cur = connection.cursor()
             cur.execute("SELECT VERSION()")
             version = cur.fetchone()
             print("Database version: {} ".format(version[0]))
 
-    def _show_tables():       
-        with self.connection:
-            cur = self.connection.cursor()
+    def _show_tables(self):      
+        connection = pymysql.connect(host=self.HOST, user=self.USER, password=self.PASSWORD, db=self.DATABASE)
+
+        with connection:
+            cur = connection.cursor()
             cur.execute("USE SASM")
             cur.execute("SHOW TABLES")
             tables = cur.fetchone()
             print(tables)
 
+    def check_user(self, username):
+        connection = pymysql.connect(host=self.HOST, user=self.USER, password=self.PASSWORD, db=self.DATABASE)
 
+        with connection:
+            cur = connection.cursor()
+            cur.execute("USE SASM")
+            cur.execute("SELECT password FROM sasm_users WHERE username = '" + username + "'")
+            user_pass = cur.fetchone()
+            return user_pass
+
+database = MariaDBImplementation()
+database._fetch_version()
+database._show_tables()
+print(database.check_user('testUser'))
