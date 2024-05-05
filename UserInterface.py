346b4971cbebcceb1a157e5451494b03546f57a7
Frantic9 <franticnine2015@gmail.com>
2024-05-05 12:50:20 -0500

    Updating reqs

--- UserInterface.py
+++ UserInterface.py
@@ -276,9 +276,6 @@ class UserInterface:
         days = input()
         self.course_controller.add_custom_course(name,start_time,end_time,days)
 
-    def get_course_review(self):
-        pass
-
     def save_schedule_to_account(self):
         pass
 
